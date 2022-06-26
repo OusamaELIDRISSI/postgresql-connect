8035c7da6c67be177e15ee94b327e0d22effb320
Ousama <elidrissi.ousama.ma@gmail.com>
2022-06-26 12:28:26 +0100

    first commit

---
+++ src/main/java/com/example/postgresql/PostgresqlConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlConnectApplication.class, args);
+	}
+
+}
